4ece77bb8eca27f5774ec75b4d99a80b5caf715f
munir-moh <munirmuhd120@gmail.com>
2024-10-27 16:15:02 -0700

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,62 @@
+#Question 1
+for i in range (100):
+    print ("Munir Mohammed")
+
+#Question 2
+for i in range (100):
+    print("Munir Mohammed", end=' ')
+for i in range (100):
+    print("Munir Mohammed")
+
+#Question 3
+for i in range (1,101):
+    print (i, "Munir Mohammed")
+
+#Qyestion 4
+for i in range (1,21):
+    print (i, "---", i**2)
+
+#Question 5
+for i in range (8,90,3):
+    print (i)
+
+#Question 6
+for i in range (100,1,-2):
+    print (i)
+
+#Question 7
+
+
+#Question 8
+name = input("Enter your name:")
+time = eval(input("Enter the times you want your name to be repetaed:"))
+for i in range (time):
+    print(name)
+
+#Question 9
+
+#Question 10
+height = eval(input("Enter the height of the box: "))
+width = eval(input("Enter the widhth of the box: "))
+for i in range (height):
+    print ("*" * width)
+
+#Question 11
+height = eval(input("Enter the height of the box: "))
+width = eval(input("Enter the width of the box: "))
+print("*" * width)
+for i in range (height - 2):
+    print("*" + " " * (width - 2 ) + "*")
+print("*" * width)
+
+#Question 12
+height = eval(input("Enter the height of the triangle: "))
+for i in range (1, height + 1):
+    print("*" * i)
+
+#Question 13
+height = eval(input("Enter the height of the triangle: "))
+for i in range (height,0,-1):
+    print("*" * i)
+
+#Question 14
